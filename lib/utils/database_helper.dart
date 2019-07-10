848d7b8eee8b083a8ce673c0d53b8113132d8995
wilson <whuanca06@gmail.com>
2019-07-10 15:15:42 -0400

    adding database columns

---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = new DatabaseHelper().internal();
+
+  factory DatabaseHelper() => _instance;
+
+  final String tableUser = "userTable";
+  final String columnId = "id";
+  final String columnUserName = "username";
+  final String columnPassword = "password";
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+    return _db;
+  }
+
+  DatabaseHelper internal() {}
+
+  initDb() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "maindb.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  /*
+    id| username| password
+    ------------------------
+    1 | wilson  | wilson
+    2 | juan    | juan
+   */
+  void _onCreate(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY, $columnUserName TEXT, $columnPassword TEXT)");
+  }
+}

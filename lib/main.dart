5f3c579a653a7d2657d8a6802ab637c6f2b97697
wilson <whuanca06@gmail.com>
2019-07-10 14:38:32 -0400

    adding dependencies an imports like paths and sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+void main() { runApp(
   new MaterialApp(
     title: 'database',
-    home: Home(),
+    home: new Home(),
   )
 );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('DataBase'),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
+      ),
+    );
+  }
+}
+
